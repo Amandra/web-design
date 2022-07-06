5be68ff172f33cf879b7d47271a8ac65793d703e
许大仙 <1900919313@qq.com>
2022-07-06 17:20:09 +0800

    🎨: 前后端交互

--- 06 前后端交互/01 Node 基础/02-fs读取文件.js
+++ 06 前后端交互/01 Node 基础/02-fs读取文件.js
@@ -6,11 +6,11 @@ const fs = require('fs');
 // 参数2：读取文件时的采用的编码格式，一般默认为 utf-8
 // 参数3：回调函数，拿到读取失败和成功的结果
 fs.readFile('./demo.txt', 'utf-8', function (err, data) {
-    // 打印失败的结果 。
-    // 如果读取成功，则 err 为 null 。
-    // 如果读取失败，则 err 为错误对象，data 为 undefined 。
-    console.log(err); // null
-    console.log('---------');
-    // 打印成功的结果
-    console.log(data);
+
+    // 判断文件是否读取成功
+    if (err) {
+        return console.log('文件读取失败' + err.message);
+    }
+
+    console.log('文件读取成功，内容是：' + data);
 });
