2b47c6f73f0d2b723b70b021104683b1dc75dea5
许大仙 <1900919313@qq.com>
2022-07-06 17:10:15 +0800

    🎨: 前后端交互

--- 06 前后端交互/01 Node 基础/01-fs读取文件.js
+++ 06 前后端交互/01 Node 基础/01-fs读取文件.js
@@ -1,5 +1,10 @@
+// 导入 fs 模块，来操作文件
 const fs = require('fs');
 
+// 调用 fs.readFile() 方法读取文件
+// 参数1：读取文件的存放路径
+// 参数2：读取文件时的采用的编码格式，一般默认为 utf-8
+// 参数3：回调函数，拿到读取失败和成功的结果
 fs.readFile('./demo.txt', 'utf-8', function (err, data) {
     console.log(err);
     console.log('---------');
