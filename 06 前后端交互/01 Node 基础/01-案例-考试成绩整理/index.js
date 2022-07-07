f7dd8b67909a7c22c76f65b7a1b958995250c997
许大仙 <1900919313@qq.com>
2022-07-07 08:46:59 +0800

    [新增功能]: 前后端交互

--- 06 前后端交互/01 Node 基础/01-案例-考试成绩整理/index.js
+++ 06 前后端交互/01 Node 基础/01-案例-考试成绩整理/index.js
@@ -2,10 +2,10 @@ const fs = require('fs');
 
 fs.readFile('./成绩.txt', 'utf-8', (err, data) => {
     if (err) {
-        return console.log('文件不存在' + err.message);
+        return console.log('读取文件失败~' + err.message);
     }
 
     console.log('文件读取成功：' + data);
 
-    
+
 });
