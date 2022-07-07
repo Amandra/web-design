c0ffe00439194203a27ef4571c17f1dc8a0eba91
许大仙 <1900919313@qq.com>
2022-07-07 16:28:30 +0800

    [新增功能]: 前后端交互

--- 06-前后端交互/02-模块化/07-demo.js
+++ 06-前后端交互/02-模块化/07-demo.js
@@ -1,1 +1,9 @@
 console.log(exports === module.exports); // true
+
+exports.username = '许大仙';
+exports.sayHi = function () {
+    console.log('你好啊');
+};
+
+console.log(exports); // { username: '许大仙', sayHi: [Function (anonymous)] }
+console.log(module.exports); // { username: '许大仙', sayHi: [Function (anonymous)] }
