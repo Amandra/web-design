ea5ece408422996239c9886011f940e87972bd8f
许大仙 <1900919313@qq.com>
2022-07-07 09:15:28 +0800

    [新增功能]: 前后端交互

--- 06 前后端交互/01 Node 基础/05-使用完整路径替换相对路径.js
+++ 06 前后端交互/01 Node 基础/05-使用完整路径替换相对路径.js
@@ -1,9 +1,10 @@
 const fs = require('fs');
 
+// 使用完整路径来防止路径动态拼接的问题
 
-fs.readFile('./demo.txt', 'utf-8', (err, data) => {
+fs.readFile(__dirname + '/demo.txt', 'utf-8', (err, data) => {
     if (err) {
-        console.log('读取文件失败：' + err.message);
+        return console.log('读取文件失败：' + err.message);
     }
 
     console.log('读取文件成功');
