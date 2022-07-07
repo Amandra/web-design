4b51386441352fa6df89e0b013b0e015b01ca1d3
许大仙 <1900919313@qq.com>
2022-07-07 16:11:01 +0800

    [新增功能]: 前后端交互

--- 06-前后端交互/02-模块化/04-module.exports对象.js
+++ 06-前后端交互/02-模块化/04-module.exports对象.js
@@ -1,1 +1,10 @@
 // 在一个自定义模块中，默认情况下，module.exports = {}
+
+// 向 module.exports 对象上挂载 username 属性
+module.exports.username = '张三';
+// 向 module.exports 对象上挂载函数
+module.exports.sayHi = function () {
+    console.log('大家好');
+};
+
+console.log(module.exports);
