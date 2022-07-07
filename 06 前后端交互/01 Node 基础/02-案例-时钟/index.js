61add32531de1de8a9193c3db5baec418c9b0b17
许大仙 <1900919313@qq.com>
2022-07-07 10:19:51 +0800

    [新增功能]: 前后端交互

--- 06 前后端交互/01 Node 基础/02-案例-时钟/index.js
+++ 06 前后端交互/01 Node 基础/02-案例-时钟/index.js
@@ -3,7 +3,7 @@
 * ② 使用 fs 模块，读取需要被处理的 HTML 文件。
 * ③ 自定义 resolveCSS 方法，来写入 index.css 样式文件。
 * ④ 自定义 resolveJS 方法，来写入 index.js 脚本文件。
-* ⑤ 自定义 resolveHTMl 方法，来写入 index.html 文件。
+* ⑤ 自定义 resolveHTML 方法，来写入 index.html 文件。
 */
 // 导入模块
 const fs = require('fs');
@@ -12,11 +12,35 @@ const path = require('path');
 const styleReg = /<style>[\s\S]*<\/style>/;
 const scriptReg = /<script>[\s\S]*<\/script>/;
 
+function resolveCSS(data) {
+    // 使用正则表达式提取页面中的 <style></style> 标签
+    const content = styleReg.exec(data);
+    // 去掉<style> 和 </style>
+    const newCSS = content[0].replace('<style>', '').replace('</style>', '');
+    // 将提取的 css 样式，写入到 index.css 文件中
+    fs.writeFile(path.join(__dirname, './clock/index.css'), newCSS, err => {
+        if (err) {
+            return console.log('文件写入失败' + err.message);
+        }
+        console.log('文件写入成功');
+    });
+}
+
+function resolveJS(data) {
+
+}
+
+function resolveHTML(data) {
+
+}
+
 // 使用 fs 模块读取文件
 fs.readFile(path.join(__dirname, './index.html'), 'utf-8', (err, data) => {
     if (err) {
         return console.log('文件读取失败');
     }
-
-
+    // 读取成功后，调用对应的方法，拆解出 css 、js 和 html
+    resolveCSS(data);
+    resolveJS(data);
+    resolveHTML(data);
 });
