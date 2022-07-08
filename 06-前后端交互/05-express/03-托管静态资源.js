4ea3e1d0b9e7267db62f81f9bd3cf10b23df4934
许大仙 <1900919313@qq.com>
2022-07-08 17:21:17 +0800

    [新增功能]: 前后端交互

---
+++ 06-前后端交互/05-express/03-托管静态资源.js
@@ -0,0 +1,10 @@
+// 导入 express
+const express = require('express');
+// 创建 WEB 服务器
+const app = express();
+// 托管静态资源
+app.use(express.static('clock'));
+// 调用 app.listen(端口，启动成功后的回调函数) ，启动服务器。
+app.listen(80, () => {
+    console.log('Express 服务器启动了，请求地址是：http://127.0.0.1');
+});
