261665cd2c915a03372c45343218278fd4cb7f4f
许大仙 <1900919313@qq.com>
2022-08-08 16:08:35 +0800

    : Vue2.x

--- 07-Vue2/03-Vue脚手架/01-vue-demo/src/components/School.vue
+++ 07-Vue2/03-Vue脚手架/01-vue-demo/src/components/School.vue
@@ -1,8 +1,8 @@
 <template>
   <!-- 组件的结构 -->
   <div class="demo">
-    <h2>学校名称：</h2>
-    <h2>学校地址：</h2>
+    <h2>学校名称：{{ name }}</h2>
+    <h2>学校地址：{{ address }}</h2>
     <button @click="showName">点我提示学校名称</button>
   </div>
 </template>
