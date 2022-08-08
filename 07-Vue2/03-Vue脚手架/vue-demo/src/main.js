ca844771fcf3ed6db516c0311577583d82b2a90d
许大仙 <1900919313@qq.com>
2022-08-08 15:16:01 +0800

    : Vue2.x

--- 07-Vue2/03-Vue脚手架/vue-demo/src/main.js
+++ 07-Vue2/03-Vue脚手架/vue-demo/src/main.js
@@ -11,7 +11,7 @@ Vue.config.productionTip = false;
 * 关于不同版本的 Vue ：
 * 1. vue.js 和 vue.runtime.xxx.js 的区别：
 *   ① vue.js 是完整版的 Vue，包含：核心功能+模板解析器。
-*   ② vue.runtime.xxx.js 是运行版的Vue，只包含：核心功能，不包含：模板解析器。
+*   ② vue.runtime.xxx.js 是运行版的 Vue，只包含：核心功能，不包含：模板解析器。
 * 2. 因为 vue.runtime.xxx.js 没有模板解析器，所以不能使用 template 配置项，需要使用 render 函数接受到的 createElement 函数去指定具体的内容。
 */
 // 创建 Vue 实例对象 -- vm
