45360c41c713c4219425cb15afb05bdfe837a284
许大仙 <1900919313@qq.com>
2022-08-08 11:11:49 +0800

    : Vue2.x

---
+++ 07-Vue2/02-Vue组件化编程/02-单文件组件/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <School></School>
+    <Student></Student>
+  </div>
+</template>
+
+<script>
+// 引入组件
+import Student from './Student.vue';
+import School from './School.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Student,
+    School
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
