e2d819b06f2e8a668bc58f1132a7e7c0053dce3b
许大仙 <1900919313@qq.com>
2022-08-08 14:20:03 +0800

    : Vue2.x

--- 07-Vue2/03-Vue脚手架/vue-demo/src/main.js
+++ 07-Vue2/03-Vue脚手架/vue-demo/src/main.js
@@ -1,8 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
+/*
+* 该文件是整个项目的入口文件
+*/
+// 引入 Vue
+import Vue from 'vue';
+// 引入 App 组件，它是所有组件的父组件
+import App from './App.vue';
+// 阻止 Vue 在启动时生成生产提示
+Vue.config.productionTip = false;
 
-Vue.config.productionTip = false
-
+// 创建 Vue 实例对象 -- vm
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    render: h => h(App),
+}).$mount('#app');
