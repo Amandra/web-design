cc5d67aa7feca9875c84f07ee25bb5edce79520f
许大仙 <1900919313@qq.com>
2022-08-09 09:42:15 +0800

    : Vue2.x

--- 07-Vue2/03-Vue脚手架/02-vue-demo-ref/src/App.vue
+++ 07-Vue2/03-Vue脚手架/02-vue-demo-ref/src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
-    <img alt="Vue logo" src="@/assets/logo.png">
-    <School/>
+    <h1 ref="title" v-text="msg"></h1>
+    <button ref="btn" @click="showDom">点我输出上方的 dom 元素</button>
+    <School ref="sch"/>
   </div>
 </template>
 
@@ -10,6 +11,18 @@ import School from '@/components/School';
 
 export default {
   name: 'App',
+  data() {
+    return {
+      msg: '欢迎学习 Vue2.x'
+    };
+  },
+  methods: {
+    showDom() {
+      console.log(this.$refs.title);
+      console.log(this.$refs.btn);
+      console.log(this.$refs.sch);
+    }
+  },
   components: {
     School,
   }
