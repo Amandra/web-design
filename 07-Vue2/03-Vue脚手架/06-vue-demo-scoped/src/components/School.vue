ced27f24eaa425451d42c7414b9cba7a45751f3d
许大仙 <1900919313@qq.com>
2022-08-09 14:48:45 +0800

    : Vue2.x

---
+++ 07-Vue2/03-Vue脚手架/06-vue-demo-scoped/src/components/School.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="demo">
+    <h2>学校名称：{{ name }}</h2>
+    <h2>学校地址：{{ address }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'School',
+  data() {
+    return {
+      name: '苏州大学',
+      address: '江苏省苏州市工业园区仁爱路199号',
+    };
+  }
+};
+</script>
+
+<style lang="less" scoped>
+.demo {
+  background-color: skyblue;
+}
+</style>
