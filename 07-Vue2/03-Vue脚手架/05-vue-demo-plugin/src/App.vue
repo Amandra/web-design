6e5c38d7965bae741f5f6292fb59179f58855d1b
许大仙 <1900919313@qq.com>
2022-08-09 14:14:47 +0800

    : Vue2.x

---
+++ 07-Vue2/03-Vue脚手架/05-vue-demo-plugin/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <img alt="Vue logo" src="@/assets/logo.png">
+    <Student/>
+    <School/>
+  </div>
+</template>
+
+<script>
+import School from '@/components/School';
+import Student from '@/components/Student';
+
+export default {
+  name: 'App',
+  components: {
+    School,
+    Student
+  }
+};
+</script>
+
+<style>
+</style>
