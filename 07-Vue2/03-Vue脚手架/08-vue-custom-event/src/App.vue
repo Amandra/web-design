6c00797bd50947665b81b16740f4834c569e9391
许大仙 <1900919313@qq.com>
2022-08-10 17:25:07 +0800

    : Vue2.x

--- 07-Vue2/03-Vue脚手架/08-vue-custom-event/src/App.vue
+++ 07-Vue2/03-Vue脚手架/08-vue-custom-event/src/App.vue
@@ -4,7 +4,8 @@
     <!-- 通过父组件给子组件传递函数类型的 props ，实现子组件给父组件传递数据。 -->
     <School :getSchoolName="getSchoolName"/>
     <!-- 通过父组件给子组件绑定一个自定义事件，实现子组件给父组件传递数据。 -->
-    <Student v-on:demo="getStudentName"/>
+    <!--<Student v-on:demo="getStudentName"/>-->
+    <Student ref="student"/>
   </div>
 </template>
 
@@ -26,6 +27,10 @@ export default {
     getStudentName(name) {
       console.log('App收到了学生的姓名：', name);
     }
+  },
+  mounted() {
+    // 绑定自定义事件
+    this.$refs.student.$on('demo', this.getStudentName);
   },
   components: {
     School,
