23293ab192f4fa0667043a40358e606fb5dc6340
许大仙 <1900919313@qq.com>
2022-08-11 09:23:34 +0800

    : Vue2.x

--- 07-Vue2/03-Vue脚手架/08-vue-custom-event/src/components/Student.vue
+++ 07-Vue2/03-Vue脚手架/08-vue-custom-event/src/components/Student.vue
@@ -4,6 +4,7 @@
     <h2>学生年龄：{{ age }}</h2>
     <button @click="sendStudentName">将学生姓名给 App 组件</button>
     <button @click="unbind">解绑 demo 事件</button>
+    <button @click="dead">销毁当前Student组件的实例（VC）</button>
   </div>
 </template>
 
@@ -26,6 +27,10 @@ export default {
       this.$off('demo'); // 解绑一个自定义事件
       // this.$off(['demo', 'hehe']); // 解绑多个自定义事件
       // this.$off();// 解绑所有的自定义事件
+    },
+    dead() {
+      //销毁了当前 Student 组件的实例，销毁后所有 Student 实例的自定义事件全部不奏效
+      this.$destroy();
     }
   }
 };
