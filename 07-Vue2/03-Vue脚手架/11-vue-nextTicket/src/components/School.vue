d46aec11796d8498acd78b7cb43fb21766c737c0
许大仙 <1900919313@qq.com>
2022-08-11 14:27:03 +0800

    : Vue2.x

---
+++ 07-Vue2/03-Vue脚手架/11-vue-nextTicket/src/components/School.vue
@@ -0,0 +1,33 @@
+<template>
+  <!-- 组件的结构 -->
+  <div class="demo">
+    <h2>学校名称：{{ name }}</h2>
+    <h2>学校地址：{{ address }}</h2>
+    <button @click="showName">点我提示学校名称</button>
+  </div>
+</template>
+
+<script>
+// 组件交互相关的代码(数据、方法等)
+export default {
+  name: 'School',
+  data() {
+    return {
+      name: '苏州大学',
+      address: '江苏省苏州市工业园区仁爱路199号',
+    };
+  },
+  methods: {
+    showName() {
+      alert(this.name);
+    }
+  }
+};
+</script>
+
+<style>
+/* 组件的样式 */
+.demo {
+  background-color: pink;
+}
+</style>
