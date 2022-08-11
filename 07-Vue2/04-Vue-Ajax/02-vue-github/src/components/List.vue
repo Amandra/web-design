d6d74cf7c48e95954f99baff3d615beff185a46b
许大仙 <1900919313@qq.com>
2022-08-11 17:17:41 +0800

    : Vue2.x

--- 07-Vue2/04-Vue-Ajax/02-vue-github/src/components/List.vue
+++ 07-Vue2/04-Vue-Ajax/02-vue-github/src/components/List.vue
@@ -1,11 +1,18 @@
 <template>
   <div class="row">
-    <div v-for="user in users" :key="user.id" class="card">
+    <!-- 展示用户列表 -->
+    <div v-for="user in users" v-show="users.length" :key="user.id" class="card">
       <a :href="user.html_url" target="_blank">
         <img :src="user.avatar_url" style='width: 100px'/>
       </a>
       <p class="card-text">{{ user.login }}</p>
     </div>
+    <!-- 展示欢迎词 -->
+    <h1 v-show="isFirst">欢迎使用</h1>
+    <!-- 展示加载中 -->
+    <h1 v-show="isLoading">加载中...</h1>
+    <!-- 展示错误信息 -->
+    <h1 v-show="errMsg">{{ errMsg }}</h1>
   </div>
 </template>
 
@@ -15,16 +22,22 @@ export default {
   name: 'List',
   data() {
     return {
+      isFirst: true, // 是否初次展示
+      isLoading: false, // 是否加载中
+      errMsg: '', // 错误信息
       users: []
     };
   },
   mounted() {
-    this.$bus.$on('users', (data) => {
-      this.users = data;
+    this.$bus.$on('updateListData', ({isFirst, isLoading, errMsg, users}) => {
+      this.isFirst = isFirst;
+      this.isLoading = isLoading;
+      this.errMsg = errMsg;
+      this.users = users;
     });
   },
   beforeDestroy() {
-    this.$bus.$off('users');
+    this.$bus.$off('getUsers');
   }
 };
 </script>
--- 07-Vue2/04-Vue-Ajax/02-vue-github/src/components/Search.vue
+++ 07-Vue2/04-Vue-Ajax/02-vue-github/src/components/Search.vue
@@ -26,10 +26,28 @@ export default {
         return alert('输入不能为空');
       }
       try {
+        // 更新前更新 List 中的数据
+        this.$bus.$emit('updateListData', {
+          isFirst: false,
+          isLoading: true,
+          errMsg: '',
+          users: []
+        });
         const {data: {items}} = await axios.get(`/github/search/users?q=${this.keyword}`);
-        this.$bus.$emit('users', items);
+        this.$bus.$emit('updateListData', {
+          isFirst: false,
+          isLoading: false,
+          errMsg: '',
+          users: items
+        });
       } catch (e) {
         console.error(e);
+        this.$bus.$emit('updateListData', {
+          isFirst: false,
+          isLoading: false,
+          errMsg: e.message,
+          users: []
+        });
       }
 
     }
