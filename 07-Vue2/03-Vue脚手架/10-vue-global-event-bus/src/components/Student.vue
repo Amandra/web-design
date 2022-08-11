390729db53b2c3d98e091663528dcb5eb336bee4
许大仙 <1900919313@qq.com>
2022-08-11 11:06:04 +0800

    : Vue2.x

---
+++ 07-Vue2/03-Vue脚手架/10-vue-global-event-bus/src/components/Student.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h2>学生姓名：{{ name }}</h2>
+    <h2>学生年龄：{{ age }}</h2>
+    <button @click="sendName">将学生姓名发送给学校组件</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Student',
+  data() {
+    return {
+      name: '许大仙',
+      age: 18
+    };
+  },
+  methods: {
+    sendName() {
+      this.$bus.$emit('SENT_NAME', this.name);
+    }
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
