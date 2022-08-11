2ce40cc76382d8929040baf7af1174de903dad22
许大仙 <1900919313@qq.com>
2022-08-11 15:22:06 +0800

    : Vue2.x

---
+++ 07-Vue2/04-Vue-Ajax/01-vue-ajax/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <button @click="getStudents">获取学生信息</button>
+    <button @click="getCars">获取汽车信息</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  methods: {
+    async getStudents() {
+      try {
+        const result = await axios.get('/api1/students');
+        console.log(result.data);
+      } catch (e) {
+        console.log(e.message);
+      }
+    },
+    async getCars() {
+      try {
+        const result = await axios.get('/api2/cars');
+        console.log(result.data);
+      } catch (e) {
+        console.log(e.message);
+      }
+    }
+  },
+};
+</script>
+
+<style>
+</style>
