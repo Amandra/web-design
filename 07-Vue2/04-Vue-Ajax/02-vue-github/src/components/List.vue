20c847edeeef8ff87b9444ec966469fd9a05cee4
许大仙 <1900919313@qq.com>
2022-08-11 17:25:40 +0800

    : Vue2.x

--- 07-Vue2/04-Vue-Ajax/02-vue-github/src/components/List.vue
+++ 07-Vue2/04-Vue-Ajax/02-vue-github/src/components/List.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="row">
     <!-- 展示用户列表 -->
-    <div v-for="user in users" v-show="users.length" :key="user.id" class="card">
+    <div v-for="user in info.users" v-show="info.users.length" :key="user.id" class="card">
       <a :href="user.html_url" target="_blank">
         <img :src="user.avatar_url" style='width: 100px'/>
       </a>
       <p class="card-text">{{ user.login }}</p>
     </div>
     <!-- 展示欢迎词 -->
-    <h1 v-show="isFirst">欢迎使用</h1>
+    <h1 v-show="info.isFirst">欢迎使用</h1>
     <!-- 展示加载中 -->
-    <h1 v-show="isLoading">加载中...</h1>
+    <h1 v-show="info.isLoading">加载中...</h1>
     <!-- 展示错误信息 -->
-    <h1 v-show="errMsg">{{ errMsg }}</h1>
+    <h1 v-show="info.errMsg">{{ info.errMsg }}</h1>
   </div>
 </template>
 
@@ -22,22 +22,21 @@ export default {
   name: 'List',
   data() {
     return {
-      isFirst: true, // 是否初次展示
-      isLoading: false, // 是否加载中
-      errMsg: '', // 错误信息
-      users: []
+      info: {
+        isFirst: true, // 是否初次展示
+        isLoading: false, // 是否加载中
+        errMsg: '', // 错误信息
+        users: []
+      }
     };
   },
   mounted() {
-    this.$bus.$on('updateListData', ({isFirst, isLoading, errMsg, users}) => {
-      this.isFirst = isFirst;
-      this.isLoading = isLoading;
-      this.errMsg = errMsg;
-      this.users = users;
+    this.$bus.$on('updateListData', (obj) => {
+      this.info = obj;
     });
   },
   beforeDestroy() {
-    this.$bus.$off('getUsers');
+    this.$bus.$off('updateListData');
   }
 };
 </script>
