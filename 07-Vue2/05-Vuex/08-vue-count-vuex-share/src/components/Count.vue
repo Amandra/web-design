a082e9a8dfe72328c05f5287b2d26079606c9f46
许大仙 <1900919313@qq.com>
2022-08-13 07:35:49 +0800

    默认插槽

---
+++ 07-Vue2/05-Vuex/08-vue-count-vuex-share/src/components/Count.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <h3>当前求和为 : {{ sum }}</h3>
+    <h3>当前求和放大 10倍 为 : {{ bigSum }}</h3>
+    <h3>我的名字是 : {{ name }}</h3>
+    <h3>我的年龄是 : {{ age }}</h3>
+    <h3 style="color: red">Person 组件的总人数是：{{ personList.length }}</h3>
+    <select v-model.number="n">
+      <option value="1">1</option>
+      <option value="2">2</option>
+      <option value="3">3</option>
+    </select>
+    <button @click="increment(n)">+</button>
+    <button @click="decrement(n)">-</button>
+    <button @click="incrementOdd(n)">当前求和为奇数再加</button>
+    <button @click="incrementWait(n)">等一等再加</button>
+  </div>
+</template>
+
+<script>
+import {mapActions, mapGetters, mapMutations, mapState} from 'vuex';
+
+export default {
+  name: 'Count',
+  data() {
+    return {
+      // sum: 0, // 当前的和
+      n: 1 // 用户选择的数字
+    };
+  },
+  computed: {
+    ...mapState(['sum', 'name', 'age', 'personList']),
+    ...mapGetters({bigSum: 'bigSum'}),
+  },
+  methods: {
+    ...mapMutations({increment: 'INCREMENT', decrement: 'DECREMENT'}),
+    ...mapActions(['incrementOdd', 'incrementWait'])
+  },
+  mounted() {
+    console.log('Count组件', mapState({sum: 'sum', name: 'name', age: 'age'}));
+  }
+};
+</script>
+
+<style scoped>
+button {
+  margin-left: 5px;
+}
+</style>
