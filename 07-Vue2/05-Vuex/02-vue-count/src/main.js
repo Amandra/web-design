75b549341b5698a4ee7242fd45e2b03ad788809d
许大仙 <1900919313@qq.com>
2022-08-12 14:49:02 +0800

    : 求和案例

--- 07-Vue2/05-Vuex/02-vue-count/src/main.js
+++ 07-Vue2/05-Vuex/02-vue-count/src/main.js
@@ -1,13 +1,9 @@
 import Vue from 'vue';
 import App from '@/App.vue';
-// 引入 Vuex
-import Vuex from 'vuex';
 // 引入 store
 import store from '@/store';
 
 Vue.config.productionTip = false;
-// 使用插件
-Vue.use(Vuex);
 
 new Vue({
     render: h => h(App),
--- 07-Vue2/05-Vuex/02-vue-count/src/store/index.js
+++ 07-Vue2/05-Vuex/02-vue-count/src/store/index.js
@@ -1,6 +1,9 @@
 // 该文件用于 Vuex 中最为核心的 store
+import Vue from 'vue';
 // 引入 Vuex
 import Vuex from 'vuex';
+// 使用插件
+Vue.use(Vuex);
 // 创建 store
 export default new Vuex.Store({
     state: { // 准备 state ，用于存储数据
