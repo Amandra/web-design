cdbce9202b679b2499e9c4e6243b1ed61f76fb16
许大仙 <1900919313@qq.com>
2022-08-12 14:45:18 +0800

    : 求和案例

---
+++ 07-Vue2/05-Vuex/02-vue-count/src/store/index.js
@@ -0,0 +1,15 @@
+// 该文件用于 Vuex 中最为核心的 store
+// 引入 Vuex
+import Vuex from 'vuex';
+// 创建 store
+export default new Vuex.Store({
+    state: { // 准备 state ，用于存储数据
+
+    },
+    actions: { // 准备 actions ，用于响应组件中的动作
+
+    },
+    mutations: { // 准备 mutations ，用于操作数据（state）
+
+    }
+});
