5bfb5fe266a43f55359613e419a4cf9b5ceb2502
许大仙 <1900919313@qq.com>
2022-08-14 20:51:25 +0800

    Vue2.x 路由

--- 07-Vue2/06-Vue路由/01-vue-router/src/App.vue
+++ 07-Vue2/06-Vue路由/01-vue-router/src/App.vue
@@ -1,9 +1,7 @@
 <template>
   <div>
     <div class="row">
-      <div class="col-xs-offset-2 col-xs-8">
-        <div class="page-header"><h2>Vue Router Demo</h2></div>
-      </div>
+      <Banner/>
     </div>
     <div class="row">
       <div class="col-xs-2 col-xs-offset-2">
@@ -11,7 +9,7 @@
           <!-- 原始 HTML 中我们使用 a 标签实现页面的跳转 -->
           <!--<a class="list-group-item active" href="./about.html">About</a>-->
           <!--<a class="list-group-item" href="./home.html">Home</a>-->
-          
+
           <!-- Vue 中借助 router-link 实现路由的切换  -->
           <router-link active-class="active" class="list-group-item" to="/about">About</router-link>
           <router-link active-class="active" class="list-group-item" to="/home">Home</router-link>
@@ -31,10 +29,11 @@
 
 <script>
 import 'bootstrap/dist/css/bootstrap.css';
+import Banner from '@/components/Banner';
 
 export default {
   name: 'App',
-  components: {}
+  components: {Banner}
 };
 </script>
 
