d85129d67a55e1e1dda55c453760a6bcaee970ce
许大仙 <1900919313@qq.com>
2022-08-14 20:57:56 +0800

    Vue2.x 路由

--- 07-Vue2/06-Vue路由/01-vue-router/src/pages/About.vue
+++ 07-Vue2/06-Vue路由/01-vue-router/src/pages/About.vue
@@ -4,7 +4,13 @@
 
 <script>
 export default {
-  name: 'About'
+  name: 'About',
+  beforeDestroy() {
+    console.log('About组件即将被销毁');
+  },
+  mounted() {
+    console.log('About组件挂载完毕');
+  }
 };
 </script>
 
--- 07-Vue2/06-Vue路由/01-vue-router/src/pages/Home.vue
+++ 07-Vue2/06-Vue路由/01-vue-router/src/pages/Home.vue
@@ -4,7 +4,13 @@
 
 <script>
 export default {
-  name: 'Home'
+  name: 'Home',
+  beforeDestroy() {
+    console.log('Home组件即将被销毁');
+  },
+  mounted() {
+    console.log('Home组件挂载完毕');
+  }
 };
 </script>
 
