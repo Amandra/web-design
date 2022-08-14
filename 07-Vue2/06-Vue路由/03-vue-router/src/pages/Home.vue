bfa7afc9f48003bf68fe8d23396d8cbbfad969d1
许大仙 <1900919313@qq.com>
2022-08-14 21:30:23 +0800

    Vue2.x 路由

--- 07-Vue2/06-Vue路由/03-vue-router/src/pages/Home.vue
+++ 07-Vue2/06-Vue路由/03-vue-router/src/pages/Home.vue
@@ -4,10 +4,10 @@
     <div>
       <ul class="nav nav-tabs">
         <li>
-          <a class="list-group-item active" href="./home-news.html">News</a>
+          <router-link active-class="active" class="list-group-item" to="/home/news">News</router-link>
         </li>
         <li>
-          <a class="list-group-item " href="./home-message.html">Message</a>
+          <router-link active-class="active" class="list-group-item" to="/home/message">Message</router-link>
         </li>
       </ul>
       <ul>
--- 07-Vue2/06-Vue路由/03-vue-router/src/pages/News.vue
+++ 07-Vue2/06-Vue路由/03-vue-router/src/pages/News.vue
@@ -1,5 +1,9 @@
 <template>
-
+  <ul>
+    <li>news001</li>
+    <li>news002</li>
+    <li>news003</li>
+  </ul>
 </template>
 
 <script>
