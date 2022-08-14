81310065c502e8f492448614c71628642d35971e
许大仙 <1900919313@qq.com>
2022-08-14 17:24:56 +0800

    Vue2.x 路由

---
+++ 07-Vue2/06-Vue路由/01-vue-router/src/router/index.js
@@ -0,0 +1,17 @@
+// 该文件专门用来创建整个应用的路由器
+// 引入 VueRouter
+import VueRouter from 'vue-router';
+// 引入组件
+import About from '@/components/About';
+import Home from '@/components/Home';
+// 创建并暴露一个路由器
+export default new VueRouter({
+    routes: [
+        {
+            path: '/about', component: About
+        },
+        {
+            path: '/home', component: Home
+        },
+    ]
+});
