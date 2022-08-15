0a8fb241e81e32fbfec4cf7da090354791391235
许大仙 <1900919313@qq.com>
2022-08-15 10:46:19 +0800

    : 路由

---
+++ 07-Vue2/06-Vue路由/09-vue-router/src/pages/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h2>我是Home的内容</h2>
+    <div>
+      <ul class="nav nav-tabs">
+        <li>
+          <router-link active-class="active" class="list-group-item" to="/home/news">News</router-link>
+        </li>
+        <li>
+          <router-link active-class="active" class="list-group-item" to="/home/message">Message</router-link>
+        </li>
+      </ul>
+      <!-- 缓存路由组件 -->
+      <keep-alive include="News">
+        <router-view></router-view>
+      </keep-alive>
+
+    </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'Home'
+};
+</script>
+
+<style scoped>
+
+</style>
