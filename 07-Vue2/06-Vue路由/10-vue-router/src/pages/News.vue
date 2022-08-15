cf3ae5f27c11f9dac4febc9ddfbdc106b2abc697
许大仙 <1900919313@qq.com>
2022-08-15 13:09:33 +0800

    : 路由

---
+++ 07-Vue2/06-Vue路由/10-vue-router/src/pages/News.vue
@@ -0,0 +1,35 @@
+<template>
+  <ul>
+    <li :style="{opacity}">欢迎学习 Vue</li>
+    <li>news001<input type="text"/></li>
+    <li>news002<input type="text"/></li>
+    <li>news003<input type="text"/></li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'News',
+  data() {
+    return {
+      opacity: 1
+    };
+  },
+  activated() {
+    this.timer = setInterval(() => {
+      console.log('@');
+      if (this.opacity <= 0) {
+        this.opacity = 1;
+      }
+      this.opacity -= 0.01;
+    }, 16);
+  },
+  deactivated() {
+    clearInterval(this.timer);
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
