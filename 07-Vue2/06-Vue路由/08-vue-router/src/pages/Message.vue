1d759ed8795e8ac1a49a60d681d727b10612ce73
许大仙 <1900919313@qq.com>
2022-08-15 10:10:20 +0800

    : 路由

---
+++ 07-Vue2/06-Vue路由/08-vue-router/src/pages/Message.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="item in messageList" :key="item.id">
+        <!-- 跳转路由，并携带 params 参数，to 的字符串写法 -->
+        <!--<router-link :to="`/home/message/detail/${item.id}/${item.title}`">{{ item.title }}</router-link>&nbsp;&nbsp;-->
+        <!-- 跳转路由，并携带 params 参数，to 的对象 -->
+        <router-link :to="{
+          name: 'detail',
+          params:{
+            id: item.id,
+            title: item.title
+          }
+        }">{{ item.title }}
+          <button @click="pushShow(item)">push查看</button>
+          <button @click="replaceShow(item)">replace查看</button>
+        </router-link>&nbsp;&nbsp;
+      </li>
+    </ul>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Message',
+  data() {
+    return {
+      messageList: [
+        {id: '001', title: '消息001'},
+        {id: '002', title: '消息002'},
+        {id: '003', title: '消息003'},
+      ]
+    };
+  },
+  methods: {
+    pushShow(item) {
+      this.$router.push({
+        name: 'detail',
+        params: {
+          id: item.id,
+          title: item.title
+        }
+      });
+    },
+    replaceShow(item) {
+      this.$router.replace({
+        name: 'detail',
+        params: {
+          id: item.id,
+          title: item.title
+        }
+      });
+    }
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ 07-Vue2/06-Vue路由/08-vue-router/src/router/index.js
@@ -0,0 +1,39 @@
+// 该文件专门用来创建整个应用的路由器
+// 引入 VueRouter
+import VueRouter from 'vue-router';
+// 创建并暴露一个路由器
+export default new VueRouter({
+    routes: [
+        {
+            path: '/about',
+            name: 'about', // 给路由命名
+            component: /* webpackChunkName: "about" */  () => import('@/pages/About')
+        },
+        {
+            path: '/home',
+            name: 'home', // 给路由命名
+            component: /* webpackChunkName: "home" */  () => import('@/pages/Home'),
+            children: [ // 通过 children 配置多级路由
+                {
+                    path: 'news',
+                    name: 'news', // 给路由命名
+                    component: /* webpackChunkName: "news" */  () => import('@/pages/News'),
+                },
+                {
+                    path: 'message',
+                    name: 'message', // 给路由命名
+                    component: /* webpackChunkName: "message" */  () => import('@/pages/Message'),
+                    children: [
+                        {
+                            path: 'detail/:id/:title', // 使用占位符声明接收params参数
+                            name: 'detail', // 给路由命名
+                            component: /* webpackChunkName: "detail" */  () => import('@/pages/Detail'),
+                            // 值为布尔值，如果布尔值为 true ，就会把路由组件收到的所有 params 参数，以 props 的形式传递给 Detail 组件
+                            props: true
+                        },
+                    ]
+                },
+            ]
+        },
+    ]
+});
