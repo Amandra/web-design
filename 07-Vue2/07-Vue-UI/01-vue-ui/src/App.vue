c66cd22601d4f2dcc2c2b66fd24cb5b059367795
许大仙 <1900919313@qq.com>
2022-08-21 10:26:51 +0800

    完整引入

--- 07-Vue2/07-Vue-UI/01-vue-ui/src/App.vue
+++ 07-Vue2/07-Vue-UI/01-vue-ui/src/App.vue
@@ -1,55 +1,36 @@
 <template>
-  <div>
-    <button>原生的按钮</button>
-    <el-row>
-      <el-button>默认按钮</el-button>
-      <el-button type="primary">主要按钮</el-button>
-      <el-button type="success">成功按钮</el-button>
-      <el-button type="info">信息按钮</el-button>
-      <el-button type="warning">警告按钮</el-button>
-      <el-button type="danger">危险按钮</el-button>
-    </el-row>
-    <el-date-picker
-        v-model="value1"
-        placeholder="选择日期"
-        type="date">
-    </el-date-picker>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <div>
+      <p>
+        If Element is successfully added to this project, you'll see an
+        <code v-text="'<el-button>'"></code>
+        below
+      </p>
+      <el-button>el-button</el-button>
+    </div>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
+import HelloWorld from './components/HelloWorld.vue';
 
 export default {
-  name: 'App',
-  data() {
-    return {
-      pickerOptions: {
-        disabledDate(time) {
-          return time.getTime() > Date.now();
-        },
-        shortcuts: [{
-          text: '今天',
-          onClick(picker) {
-            picker.$emit('pick', new Date());
-          }
-        }, {
-          text: '昨天',
-          onClick(picker) {
-            const date = new Date();
-            date.setTime(date.getTime() - 3600 * 1000 * 24);
-            picker.$emit('pick', date);
-          }
-        }, {
-          text: '一周前',
-          onClick(picker) {
-            const date = new Date();
-            date.setTime(date.getTime() - 3600 * 1000 * 24 * 7);
-            picker.$emit('pick', date);
-          }
-        }]
-      },
-      value1: '',
-    };
+  name: 'app',
+  components: {
+    HelloWorld
   }
-};
+}
 </script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
--- 07-Vue2/07-Vue-UI/01-vue-ui/src/main.js
+++ 07-Vue2/07-Vue-UI/01-vue-ui/src/main.js
@@ -2,8 +2,11 @@ import Vue from 'vue';
 import App from '@/App.vue';
 //引入 ElementUI 组件库
 import ElementUI from 'element-ui';
+
 // 引入 ElementUI 全部样式
 import 'element-ui/lib/theme-chalk/index.css';
+
+import './plugins/element.js';
 // 阻止 Vue 在启动时生成生产提示
 Vue.config.productionTip = false;
 
