2bf17507f6e327963cc0c4fa60d9f9b9670010e7
许大仙 <1900919313@qq.com>
2022-08-22 08:14:35 +0800

    ✨

---
+++ 08-TypeScript/02-TypeScript常用类型/02-原始类型.ts
@@ -0,0 +1,11 @@
+let age: number = 18;
+let username: string = '许大仙';
+let bool: boolean = true;
+let a: null = null;
+let b: undefined = undefined;
+
+console.log(age);
+console.log(username);
+console.log(bool);
+console.log(a);
+console.log(b);
