db7b48245a69c6a79adedce4f9154e97a8aa92d5
许大仙 <1900919313@qq.com>
2022-08-22 13:44:11 +0800

    ✨

--- 08-TypeScript/02-TypeScript常用类型/18-字面量类型.ts
+++ 08-TypeScript/02-TypeScript常用类型/18-字面量类型.ts
@@ -2,3 +2,8 @@ let str = 'Hello TS';
 const str2 = 'Hello TS';
 const str3: 'Hello TS' = 'Hello TS';
 let age: 18 = 18;
+
+// 字面量类型配合联合类型
+function changeDirection(direction: 'up' | 'down' | 'left' | 'right') {
+    console.log(direction);
+}
