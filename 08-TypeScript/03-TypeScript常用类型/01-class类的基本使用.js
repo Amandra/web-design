072e45fd1d9cb19931f9fd3eb14af347be4533c1
许大仙 <1900919313@qq.com>
2022-08-22 15:46:33 +0800

    🎨: TS

--- 08-TypeScript/03-TypeScript常用类型/01-class类的基本使用.js
+++ 08-TypeScript/03-TypeScript常用类型/01-class类的基本使用.js
@@ -1,7 +1,11 @@
 var Person = /** @class */ (function () {
-    function Person() {
+    // 构造函数，为类中的属性进行初始化
+    function Person(age, gender) {
+        this.gender = '男';
+        this.age = age;
+        this.gender = gender;
     }
     return Person;
 }());
-var person = new Person();
+var person = new Person(18, '男');
 console.log(person);
--- 08-TypeScript/03-TypeScript常用类型/01-class类的基本使用.ts
+++ 08-TypeScript/03-TypeScript常用类型/01-class类的基本使用.ts
@@ -1,6 +1,13 @@
 class Person {
+    age: number;
+    gender = '男';
     
+    // 构造函数，为类中的属性进行初始化
+    constructor(age: number, gender: string) {
+        this.age = age;
+        this.gender = gender;
+    }
 }
 
-const person = new Person();
-console.log(person)
+const person = new Person(18, '男');
+console.log(person);
