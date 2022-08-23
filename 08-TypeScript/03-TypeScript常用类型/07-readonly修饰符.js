51710a6e28ed3af9241b1b7260264b6cce146403
许大仙 <1900919313@qq.com>
2022-08-23 08:26:00 +0800

    ✨

---
+++ 08-TypeScript/03-TypeScript常用类型/07-readonly修饰符.js
@@ -0,0 +1,9 @@
+var Person = /** @class */ (function () {
+    function Person(age) {
+        this.age = 18;
+        this.age = age;
+    }
+    return Person;
+}());
+var person = new Person(19);
+console.log(person.age);
