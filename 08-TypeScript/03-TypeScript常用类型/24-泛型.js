c23382102e02f5dea3a39db523c8c81aa252c3a2
许大仙 <1900919313@qq.com>
2022-08-23 16:21:56 +0800

    : TS

---
+++ 08-TypeScript/03-TypeScript常用类型/24-泛型.js
@@ -0,0 +1,13 @@
+var obj = {
+    id: '1'
+};
+console.log(obj);
+var obj2 = {
+    children: [1, 2, 3]
+};
+console.log(obj2);
+var obj3 = {
+    id: '001',
+    children: [1, 2, 3]
+};
+console.log(obj3);
