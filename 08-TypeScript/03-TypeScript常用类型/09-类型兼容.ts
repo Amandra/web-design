5d516c22dd172258a3a4616c288e6d85656e7fcb
许大仙 <1900919313@qq.com>
2022-08-23 09:08:54 +0800

    : TS

---
+++ 08-TypeScript/03-TypeScript常用类型/09-类型兼容.ts
@@ -0,0 +1,20 @@
+interface Point {
+    x: number;
+    y: number;
+}
+
+interface Point2D {
+    x: number;
+    y: number;
+}
+
+interface Point3D {
+    x: number;
+    y: number;
+    z: number;
+}
+
+let p: Point;
+let p2: Point2D = p
+
+
