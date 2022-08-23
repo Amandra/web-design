08534ee6f6dce2146167e59d7c9aa459be5a25c4
许大仙 <1900919313@qq.com>
2022-08-23 11:08:35 +0800

    : TS

--- 08-TypeScript/03-TypeScript常用类型/19-泛型.js
+++ 08-TypeScript/03-TypeScript常用类型/19-泛型.js
@@ -1,7 +1,7 @@
 // 创建泛型函数
 function id(value) {
-    // console.log(value.length) // 报错
+    console.log(value.length);
     return value;
 }
 // 调用泛型函数
-id('a');
+console.log(id([1, 2, 3]));
--- 08-TypeScript/03-TypeScript常用类型/19-泛型.ts
+++ 08-TypeScript/03-TypeScript常用类型/19-泛型.ts
@@ -1,10 +1,10 @@
 // 创建泛型函数
-function id<Type>(value: Type): Type {
-    // console.log(value.length) // 报错
+function id<Type>(value: Type[]): Type[] {
+    console.log(value.length)
     return value;
 }
 
 // 调用泛型函数
-id('a')
+console.log(id([1, 2, 3]));
 
 
