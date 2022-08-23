6b14444e521262f0f0ecaa0bed081ee96ebfbedb
许大仙 <1900919313@qq.com>
2022-08-23 13:46:53 +0800

    : TS

---
+++ 08-TypeScript/03-TypeScript常用类型/08-类型兼容性.js
@@ -0,0 +1,18 @@
+// 类型兼容性
+// const arr = ['a', 'b', 'c'];
+// arr.forEach(item => { });
+// arr.forEach((item, index) => { });
+// arr.forEach((item, index, array) => { });
+var Point = /** @class */ (function () {
+    function Point() {
+    }
+    return Point;
+}());
+;
+var Point2D = /** @class */ (function () {
+    function Point2D() {
+    }
+    return Point2D;
+}());
+;
+var p = new Point2D();
