b8c052046f86e563d78c2efe6cee03059cc1953a
许大仙 <1900919313@qq.com>
2022-08-24 17:07:39 +0800

    : Vue3.x

---
+++ 09-vue3/04-vue-reactive/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <h1>我是 App 组件</h1>
+  <h2>姓名：{{ name }}</h2>
+  <h2>年龄：{{ age }}</h2>
+  <h2>工作种类：{{ job.type }}</h2>
+  <h2>工作薪水：{{ job.salary }}</h2>
+  <button @click="changeInfo">修改人的信息</button>
+</template>
+
+<script>
+
+import {ref} from 'vue';
+
+export default {
+  name: 'App',
+  setup() {
+    // 数据
+    const name = ref('张三');
+    const age = ref(18);
+    const job = ref({
+      type: '前端工程师',
+      salary: '30k'
+    });
+
+    function changeInfo() {
+      name.value = '李四';
+      age.value = 20;
+      job.value.type = 'Java工程师';
+      job.value.salary = '60k';
+      console.log('changeInfo', job.value);
+    }
+
+    return { // 返回一个对象（常用）
+      name,
+      age,
+      job,
+      changeInfo
+    };
+  }
+};
+</script>
+
+<style>
+
+</style>
