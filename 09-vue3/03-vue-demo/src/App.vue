d9f27f8238d955c9c65776c752c797d67de5d502
许大仙 <1900919313@qq.com>
2022-08-24 16:53:15 +0800

    : Vue3.x

--- 09-vue3/03-vue-demo/src/App.vue
+++ 09-vue3/03-vue-demo/src/App.vue
@@ -2,6 +2,8 @@
   <h1>我是 App 组件</h1>
   <h2>姓名：{{ name }}</h2>
   <h2>年龄：{{ age }}</h2>
+  <h2>工作种类：{{ job.type }}</h2>
+  <h2>工作薪水：{{ job.salary }}</h2>
   <button @click="changeInfo">修改人的信息</button>
 </template>
 
@@ -15,16 +17,23 @@ export default {
     // 数据
     let name = ref('张三');
     let age = ref(18);
+    let job = ref({
+      type: '前端工程师',
+      salary: '30k'
+    });
 
     function changeInfo() {
       name.value = '李四';
       age.value = 20;
-      console.log('changeInfo', name, age);
+      job.value.type = 'Java工程师';
+      job.value.salary = '60k';
+      console.log('changeInfo', job.value);
     }
 
     return { // 返回一个对象（常用）
       name,
       age,
+      job,
       changeInfo
     };
   }
--- 09-vue3/03-vue-demo/vue.config.js
+++ 09-vue3/03-vue-demo/vue.config.js
@@ -1,4 +1,19 @@
-const { defineConfig } = require('@vue/cli-service')
+const {defineConfig} = require('@vue/cli-service');
+const path = require('path');
+
+function resolve(dir) {
+  return path.join(__dirname, dir);
+}
+
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  lintOnSave: false,　　// 关闭语法检查
+  chainWebpack: config => {
+    config.resolve.alias // 配置别名
+        .set('@', resolve('src'))
+        .set('assets', resolve('src/assets'))
+        .set('components', resolve('src/components'))
+        .set('base', resolve('baseConfig'))
+        .set('public', resolve('public'));
+  },
+});
