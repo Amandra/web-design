12aad07b9cf4f2b150ed8fcaf575f2dfc960779e
许大仙 <1900919313@qq.com>
2022-08-24 11:06:18 +0800

    : TS

--- 08-TypeScript/05-react-demo/src/Hello.tsx
+++ 08-TypeScript/05-react-demo/src/Hello.tsx
@@ -1,8 +1,23 @@
+import React from "react";
+
 type Props = { name: string, age?: number }
-// 简化语法
+
 const Hello = ({name, age = 18}: Props) => {
+
+    const onClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+        console.log('button', e)
+    };
+
     return (
-        <div>你好，我的名字是：{name}，我的年龄是：{age}</div>
+        <div>
+            <div>大家好，我的名字是 {name} ，我今年 {age} 岁</div>
+            <button onClick={onClick}>
+                点赞
+            </button>
+            <input type="text" onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                console.log('input', e)
+            }}/>
+        </div>
     )
 }
 
