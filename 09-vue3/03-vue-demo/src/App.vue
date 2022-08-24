d69b162bb62ee90b9026360047ec361847e894c1
许大仙 <1900919313@qq.com>
2022-08-24 16:41:41 +0800

    : Vue3.x

--- 09-vue3/03-vue-demo/src/App.vue
+++ 09-vue3/03-vue-demo/src/App.vue
@@ -1,29 +1,31 @@
 <template>
   <h1>我是 App 组件</h1>
-  <h2>{{ name }}</h2>
-  <h2>{{ age }}</h2>
-  <button @click="sayHello">说话</button>
+  <h2>姓名：{{ name }}</h2>
+  <h2>年龄：{{ age }}</h2>
+  <button @click="changeInfo">修改人的信息</button>
 </template>
 
 <script>
 
+import {ref} from 'vue';
+
 export default {
   name: 'App',
-  // 此处只是测试一下 setup ，暂时不考虑响应式
   setup() {
     // 数据
-    let name = '张三';
-    let age = 18;
+    let name = ref('张三');
+    let age = ref(18);
 
-    // 方法
-    function sayHello() {
-      alert(`大家好，我的名字是 ${name}，我的年龄是 ${age}`);
+    function changeInfo() {
+      name.value = '李四';
+      age.value = 20;
+      console.log('changeInfo', name, age);
     }
 
     return { // 返回一个对象（常用）
       name,
       age,
-      sayHello
+      changeInfo
     };
   }
 };
