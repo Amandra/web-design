ee96d55485afc334fd7060959699d2e7dfe362e2
许大仙 <1900919313@qq.com>
2022-08-25 10:59:42 +0800

    : Vue3.x

---
+++ 09-vue3/06-vue-computed/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <Demo msg="你好啊" @hello="showHelloMsg"/>
+</template>
+
+<script>
+import Demo from '@/components/Demo';
+
+export default {
+  name: 'App',
+  components: {
+    Demo
+  },
+  setup() {
+    function showHelloMsg(value) {
+      console.log(`触发了 hello 事件，收到的参数是 ${value}`);
+    }
+
+    return {
+      showHelloMsg
+    };
+  }
+};
+</script>
+
+<style>
+
+</style>
