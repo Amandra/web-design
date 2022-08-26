b23ba4e215b354d9078e96fc07efd9575e4e0603
许大仙 <1900919313@qq.com>
2022-08-26 13:39:55 +0800

    : Vue3.x

--- 09-vue3/16-vue-teleport/src/components/Dialog.vue
+++ 09-vue3/16-vue-teleport/src/components/Dialog.vue
@@ -1,11 +1,13 @@
 <template>
   <div>
     <button @click="isShow = true">点我弹个窗</button>
-    <teleport to="html">
-      <div v-if="isShow" class="dialog">
-        <h2>我是一个弹窗</h2>
-        <h4>一些内容</h4>
-        <button @click="isShow=false">关闭弹窗</button>
+    <teleport to="body">
+      <div v-if="isShow" class="mask">
+        <div class="dialog">
+          <h2>我是一个弹窗</h2>
+          <h4>一些内容</h4>
+          <button @click="isShow=false">关闭弹窗</button>
+        </div>
       </div>
     </teleport>
   </div>
@@ -17,16 +19,28 @@ import {ref} from 'vue';
 export default {
   name: 'Dialog',
   setup() {
-
     let isShow = ref(false);
-
     return {isShow};
   }
 };
 </script>
 
 <style scoped>
+.mask {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: rgba(0, 0, 0, .5);
+}
+
 .dialog {
+  text-align: center;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
   width: 300px;
   height: 300px;
   background-color: orange;
