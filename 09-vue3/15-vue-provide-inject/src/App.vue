fb23183ce698dee7b3573bb3bb23d3269028c205
许大仙 <1900919313@qq.com>
2022-08-26 10:36:47 +0800

    : Vue3.x

--- 09-vue3/15-vue-provide-inject/src/App.vue
+++ 09-vue3/15-vue-provide-inject/src/App.vue
@@ -1,11 +1,13 @@
 <template>
-  <div>
+  <div class="app">
     <h2>我是 App 组件</h2>
+    <Demo/>
   </div>
 </template>
 
 <script>
 import Demo from '@/components/Demo';
+import {provide, reactive} from 'vue';
 
 export default {
   name: 'App',
@@ -13,11 +15,19 @@ export default {
     Demo
   },
   setup() {
+
+    const car = reactive({name: '奔驰', price: '45W'});
+
+    provide('car', car);
+
     return {};
   }
 };
 </script>
 
 <style>
-
+.app {
+  background-color: skyblue;
+  padding: 10px;
+}
 </style>
--- 09-vue3/15-vue-provide-inject/src/components/Demo.vue
+++ 09-vue3/15-vue-provide-inject/src/components/Demo.vue
@@ -1,20 +1,31 @@
 <template>
-  <div>
+  <div class="demo">
     <h2>我是 Demo 组件</h2>
+    <ul>
+      <li>汽车名称：{{ name }}</li>
+      <li>汽车价格：{{ price }}</li>
+    </ul>
   </div>
 </template>
 
 <script>
 
+import {inject, toRefs} from 'vue';
+
 export default {
   name: 'Demo',
   setup() {
-
-    return {};
+    const car = inject('car');
+    return {
+      ...toRefs(car)
+    };
   }
 };
 </script>
 
 <style scoped>
-
+.demo {
+  background-color: pink;
+  padding: 10px;
+}
 </style>
