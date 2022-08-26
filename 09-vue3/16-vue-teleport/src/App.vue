fc23cf44e64987b8e8a7b3c0a9bad6cbe7086a5e
许大仙 <1900919313@qq.com>
2022-08-26 13:14:02 +0800

    : Vue3.x

---
+++ 09-vue3/16-vue-teleport/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="app">
+    <h2>我是 App 组件</h2>
+    <Demo/>
+  </div>
+</template>
+
+<script>
+import Demo from '@/components/Demo';
+import {provide, reactive} from 'vue';
+
+export default {
+  name: 'App',
+  components: {
+    Demo
+  },
+  setup() {
+
+    const car = reactive({name: '奔驰', price: '45W'});
+
+    provide('car', car);
+
+    return {};
+  }
+};
+</script>
+
+<style>
+.app {
+  background-color: skyblue;
+  padding: 10px;
+}
+</style>
---
+++ 09-vue3/16-vue-teleport/src/components/Demo.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="demo">
+    <h2>我是 Demo 组件</h2>
+    <ul>
+      <li>汽车名称：{{ name }}</li>
+      <li>汽车价格：{{ price }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+
+import {inject, toRefs} from 'vue';
+
+export default {
+  name: 'Demo',
+  setup() {
+    const car = inject('car');
+    return {
+      ...toRefs(car)
+    };
+  }
+};
+</script>
+
+<style scoped>
+.demo {
+  background-color: pink;
+  padding: 10px;
+}
+</style>
